611e436716ccf369746fc0736e0f0b818f48c495
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-31 19:26:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+function searchRepositories() {
+  query = $('#searchTerms').val();
+  $.get('https://api.github.com/search/repositories?q=' + query, function(response) {
+    const repos = JSON.parse(this.response);
+    const src = $("#respository-template").html();
+    const template = Handlebars.compile(src);
+    const repoList = template(repos);
+    $("#results").html(repoList);
+  });
+}
+
 $(document).ready(function (){
+
+
+
 });
