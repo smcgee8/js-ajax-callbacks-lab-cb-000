bd57d6789ddfb37b008cf628c2f82df0d886a72e
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-31 23:42:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function searchRepositories() {
     const template = Handlebars.compile(src);
     const repoList = template(response.items);
     $("#results").html(repoList);
-  });
+  }).fail(displayError);
 }
 
 $(document).ready(function (){
