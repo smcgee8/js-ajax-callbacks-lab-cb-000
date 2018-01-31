a26ede0ac6c849af58cfea2af6d65961314cc698
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-31 23:24:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function showCommits(e) {
     const src = $("#commits-template").html();
     const template = Handlebars.compile(src);
     const commitsList = template(response.items);
-    console.log(response.items);
+    console.log(response);
     $("details").html(commitsList);
   });
 }
